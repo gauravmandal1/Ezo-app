168989c0c78669adb480a8842e12050d18b894d5
gauravmandal1 <64638825+gauravmandal1@users.noreply.github.com>
2023-08-29 00:56:53 +0530

    withdraw changes

--- src/components/AtmMachine.vue
+++ src/components/AtmMachine.vue
@@ -17,9 +17,16 @@
       </div>
 
       <div class="section">
+        <h2>Current Available denomination in Machine for withdrawl</h2>
+        <div class="denomination-table">
+        <div v-for="(count, denom) in availableDenomination" :key="denom" class="denom-row">
+          <span class="denom-label">{{ denom }}:</span>
+          <span class="denom-count">{{ count }}</span>
+        </div>
         <h2>Enter amount for withdrawal</h2>
         <input type="number" v-model="withdrawAmount" />
         <button class="button" @click="withdrawMoney">Withdraw</button>
+      </div>
       </div>
 
       <div class="section">
@@ -38,6 +45,12 @@ export default {
   setup() {
     // note denomination
     const denomination = ref({
+      2000: 0,
+      500: 0,
+      200: 0,
+      100: 0,
+    });
+    const availableDenomination = ref({
       2000: 0,
       500: 0,
       200: 0,
@@ -69,10 +82,26 @@ export default {
       } else if (ledger.value < withdrawAmount.value) {
         log.value.push(`[${currentTimestamp()}] Not enough funds in the ledger.`);
       } else {
-        ledger.value -= withdrawAmount.value;
-        log.value.push(`[${currentTimestamp()}] Withdraw successful of ${withdrawAmount.value}`);
+        let remainingAmount = withdrawAmount.value
+        // sorting denom notes in assending order
+        for (const denom of Object.keys(availableDenomination.value).sort((a, b) => b - a)) {
+          const notesToWithdraw = Math.floor(remainingAmount / denom);
+          if (notesToWithdraw > 0 && availableDenomination.value[denom] >= notesToWithdraw) {
+            availableDenomination.value[denom] -= notesToWithdraw;
+            remainingAmount -= notesToWithdraw * denom;
+          }
+        }
+        if (remainingAmount === 0) {
+          ledger.value -= withdrawAmount.value;
+          log.value.push(`[${currentTimestamp()}] Withdraw successful of ${withdrawAmount.value}`);
+        } else {
+          log.value.push(`[${currentTimestamp()}] Unable to withdraw exact amount.`);
+        }
       }
     };
+
+
+
     // this function will deposit money in the account
     const depositMoney = () => {
       let totalAmount = calculateTotalDeposit();
@@ -82,11 +111,18 @@ export default {
       }
       ledger.value += totalAmount;
       log.value.push(`[${currentTimestamp()}] Deposit of 2000:${denomination.value[2000]},500:${denomination.value[500]},200:${denomination.value[200]},100:${denomination.value[100]} with total of : ${totalAmount} sucessfull`);
+      for (const denom of Object.keys(denomination.value)) {
+        availableDenomination.value[denom] += denomination.value[denom];
+      }
+
+      log.value.push(`[${currentTimestamp()}] Available denomination: 2000:${availableDenomination.value[2000]},500:${availableDenomination.value[500]},200:${availableDenomination.value[200]},100:${denomination.value[100]} `);
     };
+
 
     return {
       denomination,
       withdrawAmount,
+      availableDenomination,
       log,
       calculateTotalDeposit,
       withdrawMoney,
